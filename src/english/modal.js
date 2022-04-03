f2c6f7d1d02c1552cda22705375fe2e3c50cf113
Vladimir Polukhin <vovanmozg@gmail.com>
2022-04-03 23:27:07 +0300

    Add modal verbs questions

---
+++ src/english/modal.js
@@ -0,0 +1,137 @@
+module.exports = [
+  /*
+  {
+    "question": "",
+    "type": "mc",
+    "choices": {
+      "1": "",
+      "2": "",
+      "3": ""
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+  */
+
+  /* Affirmative */
+  {
+    "question": "A: I'm so cold. B: You ___ your coat!",
+    "type": "mc",
+    "choices": {
+      "1": "should put on",
+      "2": "should to take off",
+      "3": "should take off"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+  {
+    "question": "John loves cakes, but he ___ too much or he will get fat.",
+    "type": "mc",
+    "choices": {
+      "1": "shouldn't eat",
+      "2": "should eat not",
+      "3": "should eat"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+  {
+    "question": "I'm going to visit your city. Where ___ go if I want to go shopping?",
+    "type": "mc",
+    "choices": {
+      "1": "should I",
+      "2": "I should",
+      "3": "ought to I"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+  {
+    "question": "It's raining and I don't want to get my dress wet. I ___ bring an umbrella.",
+    "type": "mc",
+    "choices": {
+      "1": "had better",
+      "2": "had better to",
+      "3": "had better not"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+  {
+    "question": "My father isn't feeling well, so I told him that he ___ to the doctor.",
+    "type": "mc",
+    "choices": {
+      "1": "ought to go",
+      "2": "ought",
+      "3": "ought to"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+    {
+    "question": "The airline only allows two pieces of luggage. You ___ pack too much or you will have to take it out at the airport!",
+    "type": "mc",
+    "choices": {
+      "1": "had better not",
+      "2": "had not better",
+      "3": "better not"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+    {
+    "question": "A: I think that the grade my teacher gave me on my test is wrong. B: Really? You ___ talk to him after class today.",
+    "type": "mc",
+    "choices": {
+      "1": "ought to",
+      "2": "should to",
+      "3": "ought"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+    {
+    "question": "I need help, doctor. My baby doesn't sleep well. What ___ do?",
+    "type": "mc",
+    "choices": {
+      "1": "should I",
+      "2": "had I better",
+      "3": "I should"
+    },
+    "answer": "1",
+    "tags": [
+      "beginner-level"
+    ],
+    "explanation": ""
+  },
+
+  /* Negative */
+
+
+  /* Question */
+]

fbc592b70271ecf19ff5a1d3d4ec9a3c24827325
vovanmozg <vovanmozg@gmail.com>
2023-03-12 08:28:08 +0400

    Add explanation for === range operator

--- src/ruby.js
+++ src/ruby.js
@@ -740,7 +740,17 @@ module.exports = [
     "tags": [
       "advanced-level"
     ],
-    "explanation": ""
+    "explanation": "In Ruby, the === operator is used for pattern matching, " +
+      "and it is implemented differently depending on the type of object " +
+      "being compared." + "\n" +
+      "When using the === operator with a range object like (1...10), it " +
+      "checks if the right-hand side object is an element within the range. " +
+      "In this case, 5 is indeed an element within the range from 1 to 10 " +
+      "(excluding 10), so the expression (1...10) === 5 returns true." + "\n" +
+      "Note that if you use the == operator instead of ===, the expression " +
+      "would return false because a range object is not equal to any of its " +
+      "elements." + "\n" +
+      "<a href='https://ruby-doc.org/core-2.7.1/Range.html#method-i-3D-3D-3D'>https://ruby-doc.org/core-2.7.1/Range.html#method-i-3D-3D-3D</a>"
   },
   {
     "question": "What will return the following code?\n<code>/b/ === /b/</code>",
